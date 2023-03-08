33d385923a300458431704692a4567abd85b2b5e
asadbek000002 <asadbektuygunov9@gmail.com>
2023-03-08 20:15:42 +0500

    add file

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import camelcase
+import requests
+url = 'http://api.weatherapi.com/v1'
+r = requests.get(url + '/current.json', params={'key':'f32b2cd7fe364abd8d8182803230603', 'q': 'Tashkent'})
+coin = r.json()
 
-c = camelcase.CamelCase()
+for i in coin:
+    print(i,':')
+    for key,value in coin[i].items():
+        print(" ", key,value)
 
-txt = "hello world"
+    print("\n")
